355047b1a09ff480780aa0dac53839f5919a6b0f
Kaik Bassanelli <kaikwb@hotmail.com>
2021-05-25 14:37:26 -0300

    fix: resolve problema no nome da funcao soma no arquivo de teste

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("\r\nResultado da soma é inválido:\r\nResultado: %d\r\nEsperado: %d", total, 30)
